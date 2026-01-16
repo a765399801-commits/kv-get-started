eb0cd864f8546ec10d21d1a7ea183f2f9aace67e
keven <a765399801@gmail.com>
2026-01-16 22:40:51 +0700

    Refactor widget writing condition
    Update condition to check for name and wxKey before writing to widgets.

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,7 @@ export default {
       const name = `微信昵称:${nickname}`
       const wxKey = `key:${openid}`
       // 3️⃣ 写入 widgets（如果客户端有）
-      if (Array.isArray(nickname)) {
+      if (name&&wxKey) {
         await env.USER_NOTIFICATION.put(
           name,
           wxKey
